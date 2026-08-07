3e935076c3ec7e7440b3d656f8d84bd97e8e857d
Dhanush Reddy <29dhanushreddy@gmail.com>
2026-08-07 11:52:07 +0000

    use neon env library

--- hello.ts
+++ hello.ts
@@ -1,17 +1,19 @@
 import { Hono } from 'hono';
 import { Pool } from 'pg';
+import { parseEnv } from '@neon/env';
+import config from './neon';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 5 });
+const env = parseEnv(config, 'hello');
+
+const pool = new Pool({ connectionString: env.postgres.databaseUrl, max: 5 });
 const app = new Hono();
 
 app.get('/', async (c) => {
   const { rows } = await pool.query('SELECT version()');
   return c.json({
-    greeting: process.env.GREETING,
+    greeting: env.function.GREETING,
     database: rows[0].version,
   });
 });
-
-app.get('/health', (c) => c.json({ status: 'ok' }));
 
 export default app;
